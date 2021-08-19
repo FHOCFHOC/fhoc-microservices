3ed102df233f0f8e48627eb654ee27964772a146
FHOCFHOC <backintheusa@gmail.com>
2021-08-19 14:56:27 -0300

    mais um commit

--- greeting-service/src/main/java/com/corinto/GreetingServiceApplication.java
+++ greeting-service/src/main/java/com/corinto/GreetingServiceApplication.java
@@ -1,4 +1,4 @@
-package com.corinto.controllers;
+package com.corinto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- greeting-service/src/main/java/com/corinto/controllers/GreetingController.java
+++ greeting-service/src/main/java/com/corinto/controllers/GreetingController.java
@@ -1,4 +1,4 @@
-package com.corinto;
+package com.corinto.controllers;
 
 import java.util.concurrent.atomic.AtomicLong;
 
